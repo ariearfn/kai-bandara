0470760050d92ac59ccc43a14e651e5da030c54d
Arie Arifin <ariearfn@gmail.com>
2024-03-14 13:35:39 +0700

    ✨ login member

--- pages/login.vue
+++ pages/login.vue
@@ -9,43 +9,28 @@ export default {
   layout: "plain",
 
   methods: {
-    handleLogin(data) {
-      // this.errorMessage = undefined;
-      // this.loading = true;
-      // this.$auth
-      //   .loginWith("local", { data: this.credentials })
-      //   .then(() => {
-      //     this.$toast.success("Welcome, " + this.$auth.user.name);
-      //     if (this.$auth.user.role === "Administrator") {
-      //       this.$router.push("/app/dashboard");
-      //     } else if (this.$auth.user.role === "Customer") {
-      //       this.$router.push("/");
-      //     }
-      //   })
-      //   .catch((err) => {
-      //     this.errorMessage = err.response.data.error.message;
-      //   })
-      //   .finally(() => (this.loading = false));
-      const token =
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTElWRS1DT0RJTkciLCJjcmVhdGVkb24iOiIyMDI0LTAzLTE0IDA5OjA1OjE4In0.q7RSjWJE6kOIkbqzWX6Nl9FB6wkbL0De4fem0MxOQC4";
-      this.$axios
-        .post(
-          "https://reservationdev.railink.co.id:8001/api/service/artsmidapp/member/login",
-          data,
-          {
-            headers: {
-              token: token,
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then((response) => {
-          console.log(response.data);
-          this.handleOtp();
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+    async handleLogin(data) {
+      try {
+        const token =
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTElWRS1DT0RJTkciLCJjcmVhdGVkb24iOiIyMDI0LTAzLTE0IDA5OjA1OjE4In0.q7RSjWJE6kOIkbqzWX6Nl9FB6wkbL0De4fem0MxOQC4";
+        const response = await this.$axios
+          .post(
+            "https://reservationdev.railink.co.id:8001/api/service/artsmidapp/member/login",
+            data,
+            {
+              headers: {
+                token: token,
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then(() => {
+            this.$toast.success("Welcome");
+            this.$router.push("/app/");
+          });
+      } catch (error) {
+        console.error("Error:", error);
+      }
     },
   },
 };
